241d086f867aeb6dc7a601981e6e662750f2ef9f
robertbutacu <robert.butacu@yahoo.com>
2018-03-23 00:16:49 +0200

    validations for Offer added function which will compute the appliance of the offer

--- src/main/scala/checkout/system/Offer.scala
+++ src/main/scala/checkout/system/Offer.scala
@@ -2,4 +2,11 @@ package checkout.system
 
 import shopping.cart.BasketItem
 
-case class Offer(item: BasketItem, eligibleNumberForItems: Int, itemsForFree: Int)
+case class Offer(item: BasketItem, eligibleNumberForItems: Int, itemsForFree: Int) {
+  type ItemsLeftToPayCount = Int
+  require(eligibleNumberForItems > 0 && itemsForFree > 0)
+
+  def applyOffer(amount: Int): ItemsLeftToPayCount = {
+    0
+  }
+}
