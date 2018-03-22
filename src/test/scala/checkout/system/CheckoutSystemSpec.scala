a44cdfb0c786d354d1107c46684b45b9f53b9c8d
robertbutacu <robert.butacu@yahoo.com>
2018-03-23 00:39:21 +0200

    final test => done

--- src/test/scala/checkout/system/CheckoutSystemSpec.scala
+++ src/test/scala/checkout/system/CheckoutSystemSpec.scala
@@ -10,6 +10,9 @@ class CheckoutSystemSpec extends FlatSpec {
   val apple = Apple()
   val orange = Orange()
   val basket = List(apple, apple, orange, apple)
+  val appleDiscount = Discount(apple, 1, 1)
+  val orangeDiscount = Discount(orange, 2, 1)
+
 
   "Given the example basket " should "return 2.05" in {
     CheckoutSystem.checkout(basket) === (2.05: BigDecimal)
@@ -18,9 +21,12 @@ class CheckoutSystemSpec extends FlatSpec {
   //  ○ buy one, get one free on Apples
   //○ 3 for the price of 2 on Oranges
   "Given the example basket " should " return  after the discount " in {
-    val appleDiscount = Discount(apple, 1, 1)
-    val orangeDiscount = Discount(orange, 2, 1)
-
     CheckoutSystem.checkoutWithOffer(basket, Set(appleDiscount, orangeDiscount)) === (1.45: BigDecimal)
+  }
+
+  "Given a basket eligible for a discount on oranges too" should "apply the discount " in {
+    val newBasket = basket ::: List(orange, orange)
+    CheckoutSystem.checkoutWithOffer(newBasket, Set(appleDiscount, orangeDiscount)) === (1.70: BigDecimal)
+
   }
 }
